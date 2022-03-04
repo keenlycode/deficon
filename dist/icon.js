14764e43e849b150ecde034a6baeed4bf24770df
Nitipit Nontasuwan <nitipit@gmail.com>
2022-03-04 16:08:52 +0700

    update

--- dist/icon.js
+++ dist/icon.js
@@ -1,7 +1,7 @@
 class $e1cc54d64b89a825$export$f04a61298a47a40f extends HTMLElement {
     constructor(){
         super();
-        this.href = this.getAttribute('href');
+        this.src = this.getAttribute('src');
         this.name = this.getAttribute('name');
         this.render();
     }
@@ -9,12 +9,12 @@ class $e1cc54d64b89a825$export$f04a61298a47a40f extends HTMLElement {
         let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         let use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
         this.innerHTML = '';
-        if (this.href) {
+        if (this.src) {
             const img = document.createElement('img');
-            img.src = this.href;
+            img.src = this.src;
             img.style.height = '1em';
             this.append(img);
-        } else {
+        } else if (this.name) {
             svg.style.width = '1em';
             svg.style.height = '1em';
             this.appendChild(svg);
