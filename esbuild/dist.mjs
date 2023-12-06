1034f0f6c6e060d32d491fd89edc7bf410e79d76
Nitipit Nontasuwan <nitipit@gmail.com>
2023-12-07 00:28:26 +0700

    update

--- esbuild/dist.mjs
+++ esbuild/dist.mjs
@@ -1,10 +1,16 @@
 import * as esbuild from 'esbuild';
 
-await esbuild.build({
-    entryPoints: ['src/icon.ts'],
+
+const result = await esbuild.build({
+    entryPoints: ['ts-src/icon.ts'],
     outfile: 'dist/icon.min.js',
     bundle: true,
+    format: "esm",
+    target: "es6",
     minify: true,
     sourcemap: true,
-    target: "chrome100",
+    keepNames: true,
+    metafile: true,
 })
+
+console.log(await esbuild.analyzeMetafile(result.metafile));
